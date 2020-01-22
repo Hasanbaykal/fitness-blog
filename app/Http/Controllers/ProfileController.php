f45ecc428e2418e9781442c71b7e8f290cd94d7a
HasanBaykal <baykalhh@gmail.com>
2020-01-22 12:23:58 +0100

    Now able to Create Profiles & Upload Profile Picture

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,10 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\URL;
+use App\Profile;
+use Auth;
 
 class ProfileController extends Controller
 {
     public function profile(){
         return view('profiles.profile');
+    }
+
+    public function addProfile(Request $request){
+        $this->validate($request, [
+            'name' => 'required',
+            'designation' => 'required',
+            'profile_pic' => ['required', 'image', 'mimes:jpg,jpeg,svg,png']
+        ]);
+        $profiles = new Profile;
+        $profiles->name = $request->input('name');
+        $profiles->user_id = Auth::user()->id;
+        $profiles->designation = $request->input('designation');
+        if(request()->has('profile_pic')){
+            $file = request()->file('profile_pic');
+            $file->move(public_path(). '/uploads/', $file->getClientOriginalName());
+            $url = URL::to("/") . '/uploads/'. $file->getClientOriginalName();
+            
+        }
+        $profiles->profile_pic = $url;
+        $profiles->save();
+        return redirect('/home')->
+        with('response', 'Profile Added Successfully');
     }
 }
--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="form-row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Profile</div>
+                <div class="card-header text-center">Profile</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,60 @@
                         </div>
                     @endif
 
-                    Profile
+                    <form method="POST" action="{{ url('/addProfile') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="form-group{{ $errors->has('') ? ' has-error' : '' }} text-center">
+                            <label for="name" class="col-md-4 col-form-label">{{ __('Enter Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>$errors->first('name')</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('designation') ? ' has-error' : '' }} text-center">
+                            <label for="designation" class="col-md-4 col-form-label">{{ __('Enter Designation') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="designation" type="input" class="form-control @error('designation') is-invalid @enderror" name="designation" required autocomplete="current-designation">
+
+                                @error('designation')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>$errors->first('designation')</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('profile_pic') ? ' has-error' : '' }} text-center">
+                            <label for="profile_pic" class="col-md-4 col-form-label">{{ __('Profile Picture') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="profile_pic" type="file" class="form-control @error('profile_pic') is-invalid @enderror" name="profile_pic" required autocomplete="current-profile_pic">
+
+                                @error('profile_pic')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>$errors->first('profile_pic')</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary btn-lg">
+                                    {{ __('Add Profile') }}
+                                </button>
+
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
