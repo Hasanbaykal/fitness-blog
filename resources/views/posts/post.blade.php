7b5a953d99e5eec8111a3899f2418f718a595a81
HasanBaykal <baykalhh@gmail.com>
2020-01-22 14:35:41 +0100

    User is now Able to Publish a BlogPost on his Profile

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -8,13 +8,82 @@
                 <div class="card-header">Post</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="row">
+                    <form method="POST" action="{{ url('/addPost') }}" enctype="multipart/form-data">
+                        @csrf
 
-                    Post
+                        <div class="form-group row">
+                            <label for="post_title" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="post_title" type="text" class="form-control @error('post_title') is-invalid @enderror" name="post_title" value="{{ old('post_title') }}" required autocomplete="post_title" autofocus>
+
+                                @error('post_title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('post_title') }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="post_body" class="col-md-4 col-form-label text-md-right">{{ __('Post Body') }}</label>
+
+                            <div class="col-md-6">
+                                <textarea rows="7" id="post_body" class="form-control @error('post_body') is-invalid @enderror" name="post_body" value="{{ old('post_body') }}" required autocomplete="post_body"></textarea>
+
+                                @error('post_body')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('post_body') }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="category_id" class="col-md-4 col-form-label text-md-right">{{ __('Category') }}</label>
+
+                            <div class="col-md-6">
+                                <select id="category_id" type="category_id" class="form-control @error('category_id') is-invalid @enderror" name="category_id">
+                                    <option value="">Select</option>
+                                    @if(count($categories) > 0)
+                                        @foreach($categories->all() as $category)
+                                            <option value="{{ $category->id }}">{{ $category->category }}</option>
+                                        @endforeach()
+                                    @endif
+                                </select>
+
+                                @error('category_id')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('category_id') }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('post_image') ? ' has-error' : '' }} text-center">
+                            <label for="post_image" class="col-md-4 col-form-label">{{ __('Featured Image') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="post_image" type="file" class="form-control @error('post_image') is-invalid @enderror" name="post_image" required autocomplete="current-post_image">
+
+                                @error('post_image')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>$errors->first('post_image')</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary btn-large">
+                                    {{ __('Publish Post') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    </div>
                 </div>
             </div>
         </div>
